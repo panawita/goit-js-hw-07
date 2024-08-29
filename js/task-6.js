152c041ac2602485b3582d17e7dba54f11f757ae
panawita <162113789+panawita@users.noreply.github.com>
2024-08-30 00:43:29 +0200

    Added code to task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const boxes = document.querySelector("div#boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+  const newBoxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    newBoxes.push(box);
+    size += 10;
+  }
+
+  boxes.append(...newBoxes);
+
+  return newBoxes;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", function () {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+document.querySelector("[data-destroy]").addEventListener("click", function () {
+  destroyBoxes();
+});
