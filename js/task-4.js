a9cfda2b9dc3cddfcbf570c6a313503df95ff33d
panawita <162113789+panawita@users.noreply.github.com>
2024-08-29 17:19:49 +0200

    Added code to task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}
